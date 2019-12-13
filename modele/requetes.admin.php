287b69ece90768b69b30b5cbd034b886aa99765f
G-Philippe <55877031+G-Philippe@users.noreply.github.com>
2019-12-13 13:35:35 +0100

    Add files via upload

--- modele/requetes.admin.php
+++ modele/requetes.admin.php
@@ -3,33 +3,9 @@
 // on récupère les requêtes génériques
 include('requetes.generiques.php');
 
-/**
-* Récupère tous les enregistrements de la table users
-* @param PDO $bdd
-* @return array
-*/
-function recupereTousUtilisateurs(PDO $bdd): array {
-$query = 'SELECT login,mot_de_passe FROM utilisateur';
-return $bdd->query($query)->fetchAll();
-}
-
 function recupereDonneesUtilisateurs(PDO $bdd): array {
     $query = 'SELECT id,nom, prenom, valeur, instant FROM utilisateur INNER JOIN mesure ON mesure.Utilisateur_id = utilisateur.id ORDER BY nom,prenom';
     return $bdd->query($query)->fetchAll();
-}
-/**
- * Ajoute un nouvel utilisateur dans la base de données
- * @param array $utilisateur
- */
-function ajouteUtilisateur(PDO $bdd, array $utilisateur) {
-
-    $query = ' INSERT INTO utilisateur (type, login, mot_de_passe,adresse_mail) VALUES (:type, :username, :password, :email)';
-    $donnees = $bdd->prepare($query);
-    $donnees->bindParam(":type", $utilisateur['type'], PDO::PARAM_STR);
-    $donnees->bindParam(":username", $utilisateur['username'], PDO::PARAM_STR);
-    $donnees->bindParam(":password", $utilisateur['password']);
-    $donnees->bindParam(":email", $utilisateur['email'], PDO::PARAM_STR);
-    return $donnees->execute();
 }
 
 /**
@@ -82,6 +58,69 @@ function recupereQuestion(PDO $bdd, $id){
 }
 
 /**
+ * Ajoute un nouveau utilisateur dans la base de données
+ * @param array $utilisateur
+ */
+function addUser(PDO $bdd, array $utilisateur)
+{
+    $query = 'INSERT INTO utilisateur (nom,prenom,date_naissance,numero_telephone,taille,poids,type,login,mot_de_passe,adresse_mail) VALUES (:nom,:prenom,:date,:telephone,:taille,:poids,:type,:login,:password,:email)';
+    $donnees = $bdd->prepare($query);
+    $donnees->bindParam(":nom", $utilisateur['nom']);
+    $donnees->bindParam(":prenom", $utilisateur['prenom']);
+    $donnees->bindParam(":date", $utilisateur['date_naissance']);
+    $donnees->bindParam(":telephone", $utilisateur['telephone']);
+    $donnees->bindParam(":taille", $utilisateur['taille']);
+    $donnees->bindParam(":poids", $utilisateur['poids']);
+    $donnees->bindParam(":type", $utilisateur['type']);
+    $donnees->bindParam(":login", $utilisateur['login']);
+    $donnees->bindParam(":password", $utilisateur['password']);
+    $donnees->bindParam(":email", $utilisateur['email']);
+    return $donnees->execute();
+}
+
+/**
+ * Supprimer une question dans la base de données
+ * @param $id
+ */
+function deleteUser(PDO $bdd, $id){
+    $query = 'DELETE FROM utilisateur WHERE id = :id ';
+    $donnees = $bdd->prepare($query);
+    $donnees->bindValue(":id", $id);
+    return $donnees->execute();
+}
+
+/**
+ * Modifier une question
+ * @param array $question
+ */
+function modifyUser(PDO $bdd, array $utilisateur){
+    $query = 'UPDATE utilisateur SET nom = :nom, prenom = :prenom, date_naissance = :date, numero_telephone = :telephone, taille= :taille, poids= :poids, type=:type, login =:login, adresse_mail=:email WHERE id = :id ';
+    $donnees = $bdd->prepare($query);
+    $donnees->bindParam(":id", $utilisateur['id']);
+    $donnees->bindParam(":nom", $utilisateur['nom']);
+    $donnees->bindParam(":prenom", $utilisateur['prenom']);
+    $donnees->bindParam(":date", $utilisateur['date_naissance']);
+    $donnees->bindParam(":telephone", $utilisateur['telephone']);
+    $donnees->bindParam(":taille", $utilisateur['taille']);
+    $donnees->bindParam(":poids", $utilisateur['poids']);
+    $donnees->bindParam(":type", $utilisateur['type']);
+    $donnees->bindParam(":login", $utilisateur['login']);
+    $donnees->bindParam(":email", $utilisateur['email']);
+    return $donnees->execute();
+}
+
+/**
+ * Recupere la question et la reponse en fonction de l'id
+ * @param $id
+ */
+function recupereUser(PDO $bdd, $id){
+    $query=$bdd->prepare('SELECT * FROM utilisateur WHERE id = :id ');
+    $query->bindValue(':id',$id);
+    $query->execute();
+    return $query->fetch();
+}
+
+/**
  * Vérifie si l'utilisateur existe dans la BDD
  * @param array $utilisateur
  */
@@ -90,33 +129,6 @@ function bddPassword(PDO $bdd, array $utilisateur) {
     $query->bindValue(':pseudo',$utilisateur['username'], PDO::PARAM_STR);
     $query->execute();
     return $query->fetch();
-}
-
-/**
- * Ajoute un nouvel utilisateur dans la base de données
- * @param array $utilisateur
- */
-function supprimerUtilisateur(PDO $bdd, array $utilisateur) {
-
-    $query = ' DELETE FROM utilisateur WHERE login=:username AND adresse_mail=:email AND type=:type';
-    $donnees = $bdd->prepare($query);
-    $donnees->bindParam(":type", $utilisateur['type'], PDO::PARAM_STR);
-    $donnees->bindParam(":username", $utilisateur['username'], PDO::PARAM_STR);
-    $donnees->bindParam(":email", $utilisateur['email'], PDO::PARAM_STR);
-    return $donnees->execute();
-}
-
-/**
- * Verifie si le login existe deja dans la BDD
- * @param array $utilisateur
- */
-function existantUtilisateur(PDO $bdd, array $utilisateur) {
-
-    $query=$bdd->prepare('SELECT adresse_mail FROM utilisateur WHERE login = :pseudo');
-    $query->bindValue(':pseudo',$utilisateur['username'], PDO::PARAM_STR);
-    $query->execute();
-    return $query->fetch();
-
 }
 
 function modifierCGU(PDO $bdd, $contenuCGU){
--- modele/requetes.generiques.php
+++ modele/requetes.generiques.php
@@ -79,4 +79,29 @@ function recupereChamp(PDO $bdd, $table, $champ){
     $query = 'SELECT '. $champ .' FROM ' . $table;
     return $bdd->query($query)->fetchAll();
 }
+
+/**
+ * Hache un mot de passe
+ * @param $mdp
+ * @return string
+ */
+function hachagePassword($mdp) : string
+{
+    $part1 = strlen($mdp);
+    $part1 = ((((($part1**$part1) + 5)/3) + 30)/$part1);
+
+    $part2 = strlen($part1.$mdp);
+    $part2 = (((($part2 + 65)/3)*$part2)**3);
+
+    $part3 = strlen($part2.$part1.$mdp.$part2);
+    $part3 = ((((($part2*$part3) + 19)/$part1) + 6)*5);
+
+    $texthash = md5($part1.$mdp.$part3);
+    $texthash2 = sha1($part2.$mdp.$part3.$texthash);
+    $texthash3 = hash('sha512', $texthash.$mdp.$texthash2.$part3);
+
+    $lol = $texthash.$texthash3.$texthash2;
+    return $lol;
+}
 ?>
+
