09789a3ef26304b283cbf2a5d6e743bac123994a
ThomasLeGarlantezec <55877060+ThomasLeGarlantezec@users.noreply.github.com>
2019-12-16 13:55:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ include("config.php");
  **/
 
 /**
- * Ceci est un test pour github semble pas marcher :( c'est triste mais ça va marcher
+ * Ceci est un test pour github semble pas marcher :( c'est triste mais ça va marcher. ça a marcher!!! maintenant on passe au second test
  */
 // Activation des erreurs
 ini_set('display_errors', 1);
