4a1e0dfa37f392e0b0f3662f1bab7fdd4c9af94d
Algonight <benjamin.fournet99@gmail.com>
2020-01-10 14:13:29 +0100

    Début des pages actionneurs

---
+++ vues/actionneur/actionneur.php
@@ -0,0 +1,36 @@
+<div id="contenuAccueil">
+    <h1>Utilisateur</h1>
+    <input type="text" id="myInput" onkeyup="triFunction()" placeholder="Search for names..">
+    <div class="blocTable">
+        <a class="ajoutBTN" href="index.php?cible=<?php echo $_SESSION['type']; ?>&fonction=ajoutActionneur"><img src="pictures/plus.png" height="32" width="32" alt="ajouter"></a>
+        <div class="overflow">
+            <table id="myTable" class="tableauFAQ">
+                <tr>
+                    <th>Id Actionneur</th>
+                    <th>Type Actionneur</th>
+                    <th>Action</th>
+                </tr>
+                <?php
+                foreach ($donneesActionneur as $element) { ?>
+                    <tr>
+                        <?php if($element['typeActionneur'] != ""){?>
+                            <td><?php echo $element['idCapteur']; ?></td>
+                            <td><?php echo $element['typeActionneur']; ?></td>
+                            <td>
+                                <div id="blocAction">
+                                    <a href="index.php?cible=<?php echo $_SESSION['type']; ?>&fonction=updateUSER"
+                                       title='Update Record'><img class="stylo" src="pictures/pencil.png" height="32" width="32"
+                                                                  alt="modifier"></a>
+                                    <a href="index.php?cible=<?php echo $_SESSION['type']; ?>&fonction=deleteUSER"
+                                       title='Delete Record'><img class="trash" src="pictures/trash.png" height="32" width="32"
+                                                                  alt="supprimer"></a>
+                                </div>
+                            </td>
+                        <?php } ?>
+                    </tr>
+                <?php } ?>
+            </table>
+        </div>
+    </div>
+</div>
+</div>
