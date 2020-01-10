b6555527b798acdcd26f35f675052fa756f2b5ea
ABCD1-1 <55496947+ABCD1-1@users.noreply.github.com>
2020-01-10 15:50:28 +0100

    design nousContacterAdmin

--- vues/backoffice/nousContacterAdmin.php
+++ vues/backoffice/nousContacterAdmin.php
@@ -1,34 +1,34 @@
-<div>
-    <div id="vide">
+<div class="conteneurNousContacter">
+        <div id="vide">
 
-    </div>
-<?php
-require(__DIR__."/../header/nousContacter.php");
-?>
-<div class="contact">
-    <div id="parametrer">
-        <form method="POST" action="">
-            <div class="input">
-                <p>
-                <input type="email" name="contenuMail" placeholder="Modifier votre mail ici">
-                <input type="submit" value="Modifier l'adresse mail">
-                </p>
-            </div>
-        </form>
+        </div>
+    <?php
+    require(__DIR__."/../header/nousContacter.php");
+    ?>
+    <div class="contact">
+        <div class="parametrer">
+            <form method="POST" action="">
+                <div class="input">
+                    <p>
+                    <input type="email" name="contenuMail" placeholder="Modifier votre mail ici">
+                    <input type="submit" value="Modifier l'adresse mail">
+                    </p>
+                </div>
+            </form>
+        </div>
+
+        <div class="parametrer">
+            <form method="POST" action="">
+                <div class="input">
+                    <p>
+                        <input type="text" name="numeroTelephone" placeholder="Modifier le numéro de téléphone ici" size="28">
+                        <input type="submit" value="Modifier le numéro de téléphone">
+                    </p>
+                </div>
+            </form>
+        </div>
     </div>
 
-    <div id="parametrer">
-        <form method="POST" action="">
-            <div class="input">
-                <p>
-                    <input type="text" name="numeroTelephone" placeholder="Modifier le numéro de téléphone ici" size="28">
-                    <input type="submit" value="Modifier le numéro de téléphone">
-                </p>
-            </div>
-        </form>
+    <div id="vide"> </div>
     </div>
-</div>
-
-<div id="vide"> </div>
-</div>
 </div>
