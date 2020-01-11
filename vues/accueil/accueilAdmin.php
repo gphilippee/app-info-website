f3b44240b8285bf059a1c5ab366bee0a47383da0
ABCD1-1 <55496947+ABCD1-1@users.noreply.github.com>
2020-01-11 21:46:31 +0100

    Update accueilAdmin.php

--- vues/accueil/accueilAdmin.php
+++ vues/accueil/accueilAdmin.php
@@ -17,11 +17,6 @@
     <h3 class="titre"><a href="index.php?cible=admin&fonction=actionneur"><span class="ruban">GESTION ACTIONNEURS</span></a></h3>
 
     <h3 class="titre"><span class="ruban">MODIFIER LES DONNÉES FIXES</span></h3>
-    <ul>
-        <li class="sous-titre"><a href="index.php?cible=admin&fonction=modifCGU">Modifier la CGU</a></li>
-        <li class="sous-titre"><a href="index.php?cible=admin&fonction=modifMentionsLegales">Modifier les mentions légales</a></li>
-        <li class="sous-titre"><a href="index.php?cible=admin&fonction=modifNousContacter">Modifier la page nous contacter</a></li>
-        <li class="sous-titre"><a href="index.php?cible=admin&fonction=modifQSN">Modifier le contenu de Qui sommes-nous</a></li>
-    </ul>
+
     <div id="margin"></div>
 </nav>
