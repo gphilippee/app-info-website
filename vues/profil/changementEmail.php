43e27b13429d6b6a3845da7c3cdbf6cef4ab861b
G-Philippe <55877031+G-Philippe@users.noreply.github.com>
2020-01-21 23:06:33 +0100

    Add files via upload

--- vues/profil/changementEmail.php
+++ vues/profil/changementEmail.php
@@ -1,10 +1,26 @@
 <div class="conteneurMail">
     <form method="post" action="">
         <h1><label for="nouvelEmail" class="profil"><?php echo _ADRESSE_EMAIL; ?></label></h1>
-        <input type="email" name="nouvelEmail" placeholder="Nouvelle adresse mail" required>
+        <input type="email" id="email" name="nouvelEmail" placeholder="Nouvelle adresse mail" required>
         <div class="blocBouton">
             <button class="styleBTN" type="submit">Valider</button>
             <a class="styleBTN addBTN" href="index.php?cible=utilisateurs&fonction=profil">Annuler</a>
         </div>
     </form>
 </div>
+
+<script>
+    function emailValid (email) {
+        return /\S+@\S+\.\S+/.test(email)
+    }
+
+    document.getElementById("email").addEventListener("input",function(e){
+        var email1=e.target.value;
+        if (emailValid(email1)){
+            e.target.style.color = "green";
+        }
+        else{
+            e.target.style.color = "red";
+        }
+    });
+</script>
--- vues/profil/changementMdp.php
+++ vues/profil/changementMdp.php
@@ -5,10 +5,10 @@
         <input type="password" name="ancienMdp" placeholder="Ancien mot de passe" required>
 
         <label for="nouveauMdp"><?php echo _NOUVEAUMDP; ?></label>
-        <input type="password" name="nouveauMdp" placeholder="Nouveau mot de passe" required>
+        <input type="password" id="mdp1"  name="nouveauMdp" placeholder="Nouveau mot de passe" required>
 
         <label for="confirmationNouveauMdp"><?php echo _CMDP; ?></label>
-        <input type="password" name="confirmationNouveauMdp" placeholder="Confirmer votre nouveau mot de passe" required>
+        <input type="password" id="mdp2" name="confirmationNouveauMdp" placeholder="Confirmer votre nouveau mot de passe" required>
 
         <div class="blocBouton">
             <input type="submit" value="Valider"/>
@@ -16,3 +16,27 @@
         </div>
     </form>
 </div>
+
+<script>
+
+    document.getElementById("mdp1").addEventListener("input",function (e){
+        var mdp1=e.target.value;
+        if (mdp1.length>7){
+            e.target.style.color ="green";
+        }
+        else{
+            e.target.style.color ="red";
+        }
+    });
+
+    document.getElementById("mdp2").addEventListener("input",function (e){
+        var mdp1=document.getElementById("mdp1").value;
+        var mdp2=e.target.value;
+        if (mdp1==mdp2){
+            e.target.style.color ="green";
+        }
+        else{
+            e.target.style.color ="red";
+        }
+    });
+</script>
