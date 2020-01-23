a0893f1473801f3a1426163dc306d57f3a76dda3
Algonight <benjamin.fournet99@gmail.com>
2020-01-23 11:26:44 +0100

    modif actionneur

--- config.php
+++ config.php
@@ -5,19 +5,24 @@ if(isset($_COOKIE['lang'])){
 }else{
     setcookie("lang", "fr", time()+3600);  /* expire dans 1 heure */
 }
-
-switch($_COOKIE['lang']){
-    case "fr":
-        $fichier_langage = "traduction/fr.inc";
-        $drapeau="pictures/drapeau_FR.png";
-        break;
-    case "en":
-        $fichier_langage = "traduction/en.inc";
-        $drapeau="pictures/drapeau_EN.png";
-        break;
-    default :
-        $fichier_langage = "traduction/fr.inc";
-        $drapeau="pictures/drapeau_FR.png";
+if(isset($_COOKIE['lang'])) {
+    switch ($_COOKIE['lang']) {
+        case "fr":
+            $fichier_langage = "traduction/fr.inc";
+            $drapeau = "pictures/drapeau_FR.png";
+            break;
+        case "en":
+            $fichier_langage = "traduction/en.inc";
+            $drapeau = "pictures/drapeau_EN.png";
+            break;
+        default :
+            $fichier_langage = "traduction/fr.inc";
+            $drapeau = "pictures/drapeau_FR.png";
+    }
+}
+else{
+    $fichier_langage = "traduction/fr.inc";
+    $drapeau = "pictures/drapeau_FR.png";
 }
 
 require($fichier_langage);
